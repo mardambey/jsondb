b2b6adac80af017a9592defbefb820c9c902771f
Hisham Mardam-Bey <hisham.mardambey@gmail.com>
2013-04-06 15:53:21 -0400

    --TODO: return JSON or JSONP

--- src/main/scala/net/mardambey/jsondb/JsonDB.scala
+++ src/main/scala/net/mardambey/jsondb/JsonDB.scala
@@ -20,7 +20,7 @@ import java.util.concurrent.TimeoutException
 
 object JsonDB extends App {
   val log = java.util.logging.Logger.getLogger(getClass.getName)
-  val SQLQuery = """/query\?sql=(.*)""".r
+  val SQLQuery = """/query\.jsonp?\?sql=(.*)""".r
   val QSTORE_CLASS = "jsondb.qstore.class"
   val HTTPD_PORT = 8080
   implicit val timeout = Timeout(600 seconds)
@@ -38,31 +38,33 @@ object JsonDB extends App {
       case e:Exception => log.severe("Failed while loading QueryStore configuration: %s - %s\n%s".format(e.getClass, e.getMessage, e.getStackTraceString))
     }
   }
-  
-  def foo(a:Any) : Option[_] = {None}
-  
+      
   log.info("Binding Http server to port %s".format(HTTPD_PORT))
+  
   val httpd = new HttpServer(HTTPD_PORT)
-  	httpd.addHandler("/query", new net.mardambey.jsondb.HttpServer.RequestHandler() {
-  	  
-	  def handle(req:HttpRequest) : String = {
-	    try {
-		    req.getUri() match {
-		      case SQLQuery(q) => {
-		        val future = Database() ? Load(Query(URLDecoder.decode(q, "UTF-8"), alias=None, refreshInterval=0), refresh = false)
-		        val result = Await.result(future, timeout.duration).asInstanceOf[Result]
-		        "jsonp(%s)".format(result.toJson)	        
-		      }
-		      case u => "unknown: %s".format(u)
-		    }
-	    } catch {
-	      case e:TimeoutException => {
-	        log.warning("Query taking more than 600 seconds: %s".format(req.getUri))
-	        "Query taking more than 600 seconds: %s".format(req.getUri)
-	      }
-	    }
-	  }	   
+  	.addHandler("/query.jsonp", new net.mardambey.jsondb.HttpServer.RequestHandler() {
+  	  def handle(req:HttpRequest) : String = "jsonp(%s)".format(handleRequest(req))
+    })
+    .addHandler("/query.json", new net.mardambey.jsondb.HttpServer.RequestHandler() {
+	  def handle(req:HttpRequest) : String = handleRequest(req)
 	})
 	
-	httpd.start()
-}
+  httpd.start()
+  
+  def handleRequest(req:HttpRequest) : String = try {
+    req.getUri() match {
+      case SQLQuery(q) => {
+        val future = Database() ? Load(Query(URLDecoder.decode(q, "UTF-8"), alias=None, refreshInterval=0), refresh = false)
+        val result = Await.result(future, timeout.duration).asInstanceOf[Result]
+        result.toJson
+      }
+      
+      case u => """"["unknown":"%s"]""".format(u)
+    }
+  } catch { 
+    case e:TimeoutException => {
+      log.warning("Query taking more than 600 seconds: %s".format(req.getUri))
+      """"["Query taking more than 600 seconds": "%s"]""".format(req.getUri)
+    }
+  }
+}	
